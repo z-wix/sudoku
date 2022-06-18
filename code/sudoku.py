e3904f2d63d935c07119e7ddbc086167b255b0f7
Zack Wixom <zackwixom@gmail.com>
2022-06-18 17:11:34 -0600

    started script

---
+++ code/sudoku.py
@@ -0,0 +1,19 @@
+"""
+This is my Automatic Sudoku Script!
+
+The idea is that it will loop there the empty values
+and assign it a temporary value based on the rules 
+of Sudoku. If there is only one possible value, then
+it will assign that value. The script will keep looping
+thru all the empty values until it is filled in.
+
+This script will just be the looping funciton that will
+be used by another scipr to fill in the data.
+
+This is a WORK IN PROGRESS
+
+"""
+
+import pandas as pd
+import plotly.express as px
+
