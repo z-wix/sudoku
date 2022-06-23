0c9507cbb8fe3688d718d727c704f224e68f64d4
Zack Wixom <zackwixom@gmail.com>
2022-06-23 14:40:30 -0600

    renamed file and updated

---
+++ code/sudoku_columns.py
@@ -0,0 +1,120 @@
+"""
+This is my Automatic Sudoku Script!
+
+The idea is that it will loop there the empty values
+and assign it a temporary value based on the rules 
+of Sudoku. If there is only one possible value, then
+it will assign that value. The script will keep looping
+thru all the empty values until it is filled in.
+
+This script will just be the looping funciton that will
+be used by another scipr to fill in the data.
+
+This is a WORK IN PROGRESS
+
+"""
+
+import pandas as pd
+import plotly.express as px
+
+# Test using a Matrix
+sudoku_matrix = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+
+    [0, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9, 1, 2, 3, 4, 0, 6, 7],
+
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 0, 1, 2, 3, 4, 5],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8]
+    ]
+
+
+print(sudoku_matrix)
+
+# Test using a dataset
+sudoku_df = pd.DataFrame(
+    sudoku_matrix,
+    columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+)
+
+sudoku_df.to_csv("../input/simple_sudoku.csv")
+
+
+# Functions
+
+def find_missing(lst):
+    return [x for x in range(1,10) if x not in lst]
+
+def add_char(char, incr = 1):
+    int = ord(char[0])
+    next_char = chr(int + incr)
+    return next_char
+
+def find_sector_columns(current_column):
+    # possible column sectors
+    columns1 = ['A', 'B', 'C']
+    columns2 = ['D', 'E', 'F']
+    columns3 = ['G', 'H', 'I']
+
+    # Loop to determin which column sector it is in
+    if current_column in columns1:
+        return columns1
+    elif current_column in columns2:
+        return columns2
+    else:
+        return columns3
+
+
+def find_sector_rows(current_row):
+    # possible row sectors
+    rows1 = ['a', 'b', 'c']
+    rows2 = ['d', 'e', 'f']
+    rows3 = ['g', 'h', 'i']
+
+    # Loop to determin which row sector it is in
+    if current_row in rows1:
+        return rows1
+    elif current_row in rows2:
+        return rows2
+    else:
+        return rows3
+
+
+# Test to split into a 3x3 dataset
+cols = list(sudoku_df.columns)
+rows = list(sudoku_df.index)
+
+#Empty Dataframe to fill in data with
+output_matrix = []
+finished_sudoku = pd.DataFrame(
+    output_matrix,
+    columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+)
+
+# Loop testing column orientation
+for i in cols:
+    print(f'Colummn {i}')
+    column = sudoku_df[i]
+    for j in rows:
+        print(f'index {j}')
+        value = column.loc[j]
+        print(f'the value is {value}')
+        if value == 0:
+            x_value = find_missing(column.values)
+            print(f'the missing values are {x_value}')
+            if len(x_value) == 1:
+                finished_sudoku.loc[j,i] = x_value
+        else:
+            finished_sudoku.loc[j,i] = value
+
+finished_sudoku
+
+finished_sudoku.to_csv("../output/single_column_result.csv")
+
+# sudoku_df[['A', 'B', 'C']].iloc[[0, 1, 2]]
