1f9e9aceda71cb26354a0a5fceb2bd4143c444cf
Zack Wixom <zackwixom@gmail.com>
2022-06-24 14:58:07 -0600

    updated with new module capability

--- code/sudoku_columns.py
+++ code/sudoku_columns.py
@@ -16,74 +16,10 @@ This is a WORK IN PROGRESS
 
 import pandas as pd
 import plotly.express as px
+from sudoku_module import find_missing, generate_sudoku
 
-# Test using a Matrix
-sudoku_matrix = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [4, 5, 6, 7, 8, 9, 1, 2, 3],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-
-    [0, 3, 4, 5, 6, 7, 8, 9, 1],
-    [5, 6, 7, 8, 9, 1, 2, 3, 4],
-    [8, 9, 1, 2, 3, 4, 0, 6, 7],
-
-    [3, 4, 5, 6, 7, 8, 9, 1, 2],
-    [6, 7, 8, 0, 1, 2, 3, 4, 5],
-    [9, 1, 2, 3, 4, 5, 6, 7, 8]
-    ]
-
-
-print(sudoku_matrix)
-
-# Test using a dataset
-sudoku_df = pd.DataFrame(
-    sudoku_matrix,
-    columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-)
-
-sudoku_df.to_csv("../input/simple_sudoku.csv")
-
-
-# Functions
-
-def find_missing(lst):
-    return [x for x in range(1,10) if x not in lst]
-
-def add_char(char, incr = 1):
-    int = ord(char[0])
-    next_char = chr(int + incr)
-    return next_char
-
-def find_sector_columns(current_column):
-    # possible column sectors
-    columns1 = ['A', 'B', 'C']
-    columns2 = ['D', 'E', 'F']
-    columns3 = ['G', 'H', 'I']
-
-    # Loop to determin which column sector it is in
-    if current_column in columns1:
-        return columns1
-    elif current_column in columns2:
-        return columns2
-    else:
-        return columns3
-
-
-def find_sector_rows(current_row):
-    # possible row sectors
-    rows1 = ['a', 'b', 'c']
-    rows2 = ['d', 'e', 'f']
-    rows3 = ['g', 'h', 'i']
-
-    # Loop to determin which row sector it is in
-    if current_row in rows1:
-        return rows1
-    elif current_row in rows2:
-        return rows2
-    else:
-        return rows3
-
+# Load in Sudoku difficulty = 1
+sudoku_df = generate_sudoku()
 
 # Test to split into a 3x3 dataset
 cols = list(sudoku_df.columns)
@@ -93,27 +29,28 @@ rows = list(sudoku_df.index)
 output_matrix = []
 finished_sudoku = pd.DataFrame(
     output_matrix,
-    columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+    columns = cols,
+    index = rows
 )
 
 # Loop testing column orientation
+print("Solving by Column")
 for i in cols:
-    print(f'Colummn {i}')
+    #print(f'Colummn {i}')
     column = sudoku_df[i]
     for j in rows:
-        print(f'index {j}')
+        #print(f'index {j}')
         value = column.loc[j]
-        print(f'the value is {value}')
+        #print(f'the value is {value}')
         if value == 0:
             x_value = find_missing(column.values)
-            print(f'the missing values are {x_value}')
+            #print(f'the missing values are {x_value}')
             if len(x_value) == 1:
                 finished_sudoku.loc[j,i] = x_value
         else:
             finished_sudoku.loc[j,i] = value
 
-finished_sudoku
+print(finished_sudoku)
 
 finished_sudoku.to_csv("../output/single_column_result.csv")
 
